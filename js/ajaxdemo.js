af03abd049b6f5ac05cbfd7b58445606bb7ba77f
ahamedsahib <adsahib39@gmail.com>
2021-08-27 14:25:02 +0530

    [Ahamed]Add:Display data in web Page from JSON server

--- js/ajaxdemo.js
+++ js/ajaxdemo.js
@@ -1,9 +1,9 @@
-let XMLHttpRequest = require('xmlhttprequest').XMLHttpRequest;
+//let XMLHttpRequest = require('xmlhttprequest').XMLHttpRequest;
 function makePromiseCall(methodType,url,async=true,data=null)
 {
     return new Promise(function(resolve,reject){
         let xhr = new XMLHttpRequest();
-        xhr.onreadystatechange=function(){
+        xhr.onload=function(){
             //console.log("State Change Called. Ready State: "+xhr.readyState+" Status: "+xhr.status);
             if(xhr.readyState ==4){
                 if(xhr.status==200||xhr.status==201){
@@ -27,18 +27,18 @@ function makePromiseCall(methodType,url,async=true,data=null)
     });
 }
 //get using get method and call back
-const getURL = "http://localhost:3000/employees";
+const getURL = "http://localhost:3000/employees/2";
 makePromiseCall("GET",getURL,true)
-.then(responseText=>{console.log("Get User Data: "+responseText)})
-.catch(error=>console.log("GET error status: "+JSON.stringify(error)));
+    .then(responseText=>{document.querySelector("#get-services").textContent = "User data: "+responseText;}).
+    catch(error=>{document.querySelector("#get-services").textContent = "Get Error status: "+JSON.stringify(error);});
 //delete data cdusing delete method and call back
 const delURL = "http://localhost:3000/employees/4";
 makePromiseCall("DELETE",delURL,false)
-.then(responseText=>{console.log("User deleted : "+responseText)})
-.catch(error=>console.log("GET error status: "+JSON.stringify(error)))
+    .then(responseText=>{document.querySelector("#delete-services").textContent = "Deleted data "}).
+    catch(error=>{document.querySelector("#delete-services").textContent = "Delete Error status:: "+JSON.stringify(error);})
 //Add data using post method and call back
 const postURL = "http://localhost:3000/employees";
 const empData = {"first_name":"Lionel","last_name":"Messi","salary":50000};
 makePromiseCall("POST",postURL,true,empData)
-.then(responseText=>{console.log("User Added : "+responseText)})
-.catch(error=>console.log("GET error status: "+JSON.stringify(error)))
+    .then(responseText=>{document.querySelector("#post-services").textContent = "Added data: "+responseText;})
+    .catch(error=>{document.querySelector("#post-services").textContent = "Post error status: "+JSON.stringify(error);})
